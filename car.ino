3de173ca152d9a40867ba6f82e0d56b8a719d9d7
Vanesche Luca <luca.vanesche@leerling.pti.be>
2018-03-12 16:17:46 +0100

    added max min on ir sensor

--- car.ino
+++ car.ino
@@ -4,15 +4,72 @@
 int           ledState       = LOW;
 unsigned long calibrateDelay = 2000;
 
-const byte snr_light1_smoothnumber = 150;
+const byte snr_light1_smoothnumber = 50;
 int snr_light1_readings[snr_light1_smoothnumber];
 int snr_light1readIndex = 0;
 int snr_light1total = 0;
 int snr_light1average = 0;
 
+float out;
+
+void getAvg() {
+  int min = 1024;
+  int max = 0;
+
+  int m = 15;
+  int i = 0;
+  int tot = 0;
+  int readings[m];
+  while (i < m || true) {
+    debouceTick();
+    if (btnState == HIGH && btnFlank) {
+      min = 1024;
+      max = 0;
+        }
+
+    if (i >= m) {
+      i = 0;
+      tot = 0;
+    }
+
+      if (!out) out = analogRead(snr_light1);
+      out += (analogRead(snr_light1) - out) * 0.3;
+
+      readings[i] = out;
+
+      tot += out;
+
+      int t = tot / (i + 1);
+      if (min > t) min = t;
+      if (max < t) max = t;
+
+      Serial.print(out);
+      Serial.print(", ");
+      Serial.print(min);
+      Serial.print(", "),
+      Serial.print(max);
+      Serial.print(", "),
+      Serial.print((max + min) / 2);
+      digitalWrite(led, (max + min) / 2 < t);
+      Serial.print(", ");
+      Serial.println(t);
+
+        i++;
+  }
+
+  while (true) {
+    Serial.print(out);
+    Serial.print(", ");
+    Serial.println(tot / i);
+  }
+}
+
 void smoothRun() {
+  if (!out) out = analogRead(snr_light1);
+  out += (analogRead(snr_light1) - out) * 0.3;
+
   snr_light1total = snr_light1total - snr_light1_readings[snr_light1readIndex];
-  snr_light1_readings[snr_light1readIndex] = analogRead(snr_light1);
+  snr_light1_readings[snr_light1readIndex] = out;
   snr_light1total = snr_light1total + snr_light1_readings[snr_light1readIndex];
   snr_light1readIndex = snr_light1readIndex + 1;
 
@@ -58,6 +115,10 @@ void loop() {
       }
   digitalWrite(led, ledState);
 
+  getAvg();
+
   smoothRun();
-  Serial.println(analogRead(snr_light1average));
+  Serial.print(out);
+  Serial.print(", ");
+  Serial.println(snr_light1average);
 }
